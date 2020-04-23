a47b4e18e49a533bbf26ceb798031cf9d9484027
Matrixgek91 <jschouw1@gmail.com>
2020-04-23 12:09:52 +0200

    Added toolbar and sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+
+/* Materials components imports */
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+/* End materials imports */
+
+
+/*const appRoutes: Routes = [
+
+]*/
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
+    // Routing
+    /*RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} //<-- Debugging only!
+    ),*/
+    // End of Routing
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //Material imports
+    MatSliderModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+    //End of meterial imports
   ],
   providers: [],
   bootstrap: [AppComponent]
